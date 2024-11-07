bbd704ddfc61cb6c924c1625b851942e58e5ca67
roma <bshs71734@gmail.com>
2024-11-07 09:49:27 +0300

    Task 3 complete

--- ipo-lr9-3/collision.py
+++ ipo-lr9-3/collision.py
@@ -28,8 +28,8 @@ def isCollisionRect(list1,list2):
     elif correct_error == False:
         all_points_list1=[(i,j) for i in range(int(numX1),int(numX2)+1) for j in range(int(numY1),int(numY2)+1)]  #Список для всего поля 1 прямоугольника
         all_points_list2=[(i,j) for i in range(int(numX3),int(numX4)+1) for j in range(int(numY3),int(numY4)+1)]  #Список для всего поля 2 прямоугольника
-        print(list2)
         print(all_points_list1)
+        print(all_points_list2)
         if (list2[0] in all_points_list1  or  list2[1] in all_points_list1) or (list1[0] in all_points_list2  or  list1[1] in all_points_list2) :  #Проверка нахождения 1 прямоугольника во 2 или наоборот
             return True
         else:
