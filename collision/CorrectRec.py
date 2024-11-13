3fecc433d4f942cb6154b5484ae22f8bd4943e3d
roma <bshs71734@gmail.com>
2024-11-13 08:34:41 +0300

    Task 2 complete

--- collision/CorrectRec.py
+++ collision/CorrectRec.py
@@ -1,7 +1,17 @@
-def isCorrectRect(c):
-    if c[0][0] < c[1][0] and c[0][1]< c[1][1]:
-        return True
-    else:
-        return False
+def isCorrectRect():
+    counter = 0 #показывает, в каком из прямоугольниов ошибка 
+    
+    class RectCorrectError(Exception):  #Класс для ошибки 
+        pass
 
 
+    for i in rectangles: #перебор всех прямоугольничков
+        counter += 1
+        try: 
+            if i[0][0] >= i[1][0] or i[0][1] >= i[1][1]:
+                raise RectCorrectError(f'Один из прямоугольников некорректный номер {counter}')
+        except RectCorrectError as e:
+            print(e)
+            return False  
+
+    return True  
